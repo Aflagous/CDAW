19d78e0204863c080a0e4d4be987442a9ee9c1c8
baptiste <86311564+Aflagous@users.noreply.github.com>
2024-03-11 08:45:27 +0100

    wip

---
+++ jalon1/exo01/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Heure Courante</title>
+</head>
+<body>
+    <h1>Heure Courante</h1>
+    <p>L'heure actuelle est : <?php echo date("H:i:s"); ?></p>
+</body>
+</html>
